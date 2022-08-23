d6bb897a4d82a8f7d1eb4af76b32b1ee8e6d5468
Daniel Lautzenheiser <lautzd@gmail.com>
2022-08-23 16:34:40 -0400

    Fix some linting issues

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Twitter from "../assets/twitter-alt.svg";
-import GitHub from "../assets/github-alt.svg";
-import config from "../lib/config";
+import React from 'react';
+import Twitter from '../assets/twitter-alt.svg';
+import GitHub from '../assets/github-alt.svg';
+import config from '../lib/config';
 
 export function SocialList({}) {
   return (
@@ -10,17 +10,12 @@ export function SocialList({}) {
         title="Twitter"
         href={`https://twitter.com/${config.twitter_account}`}
         target="_blank"
-        rel="noopener"
+        rel="noopener noreferrer"
       >
-        <Twitter width={24} height={24} fill={"#222"} />
+        <Twitter width={24} height={24} fill={'#222'} />
       </a>
-      <a
-        title="GitHub"
-        href={`https://github.com/${config.github_account}`}
-        target="_blank"
-        rel="noopener"
-      >
-        <GitHub width={24} height={24} fill={"#222"} />
+      <a title="GitHub" href={`https://github.com/${config.github_account}`} target="_blank" rel="noopener noreferrer">
+        <GitHub width={24} height={24} fill={'#222'} />
       </a>
       <style jsx>{`
         a {
--- src/util/useLocation.ts
+++ src/util/useLocation.ts
@@ -1,9 +1,11 @@
-import { useMemo } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useMemo } from 'react';
 
 export default function useLocation(): Partial<Location> {
-  if (typeof window === "undefined") {
-    return {};
-  }
-
-  return useMemo(() => window?.location, [window?.location]);
+  return useMemo(() => {
+    if (typeof window === 'undefined') {
+      return {};
+    }
+    return window?.location;
+  }, [window?.location]);
 }
